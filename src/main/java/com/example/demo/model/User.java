0bfa853daa57bccd5b53cb71373a5e85fd6cf030
Иван <streeeees13.s@gmail.com>
2023-07-29 19:28:43 +0300

    boot

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,25 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String surname;
+
+    @Column
+    private int age;
+
+}
---
+++ src/main/java/com/example/demo/service/UserServiceImp.java
@@ -0,0 +1,38 @@
+package com.example.demo.service;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class UserServiceImp implements UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImp(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        return userRepository.getOne(id);
+    }
+}
